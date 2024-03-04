3307a705fe2511a4d48182555849a7ffae655c5b
radude89 <radu.ionut.dan@gmail.com>
2024-03-04 06:52:09 +0200

    Add link for demo project

--- codelabs/DessertClicker/app/src/main/java/com/example/dessertclicker/MainUIState.kt
+++ codelabs/DessertClicker/app/src/main/java/com/example/dessertclicker/MainUIState.kt
@@ -1,5 +1,6 @@
 package com.example.dessertclicker
 
+// https://github.com/google-developer-training/basic-android-kotlin-compose-training-dessert-clicker/blob/viewmodel/app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
 data class MainUIState (
     val revenue: Int = 0,
     val dessertsSold: Int = 0,
