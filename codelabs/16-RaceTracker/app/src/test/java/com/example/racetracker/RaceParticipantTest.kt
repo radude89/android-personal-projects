2d84081239f4ca6877ea3b1da412a8de617c4ed7
radude89 <radu.ionut.dan@gmail.com>
2024-08-26 05:48:39 +0300

    Implemente code lab assignments

--- codelabs/16-RaceTracker/app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ codelabs/16-RaceTracker/app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -16,7 +16,15 @@
 package com.example.racetracker
 
 import com.example.racetracker.ui.RaceParticipant
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class RaceParticipantTest {
     private val raceParticipant = RaceParticipant(
         name = "Test",
@@ -25,4 +33,47 @@ class RaceParticipantTest {
         initialProgress = 0,
         progressIncrement = 1
     )
+
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        launch {
+            raceParticipant.run()
+        }
+        advanceTimeBy(
+            raceParticipant.maxProgress * raceParticipant.progressDelayMillis
+        )
+        runCurrent()
+
+        assertEquals(100, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        assertEquals(0, raceParticipant.currentProgress)
+
+        launch {
+            raceParticipant.run()
+        }
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+        runCurrent()
+
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RaceResetted_ProgressIsZero() = runTest {
+        launch {
+            raceParticipant.run()
+        }
+        advanceTimeBy(
+            raceParticipant.maxProgress * raceParticipant.progressDelayMillis
+        )
+        launch {
+            raceParticipant.reset()
+        }
+        runCurrent()
+
+        assertEquals(0, raceParticipant.currentProgress)
+    }
 }
